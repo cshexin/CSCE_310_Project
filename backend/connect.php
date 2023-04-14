0900336f4b8aa272c58cac2b6df84d65fcff99e4
cshexin <cshexin@outlook.com>
2023-04-13 21:32:30 -0500

    connected database

--- backend/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $user = 'root';
-    $pass = '';
-    $db = 'csce_310_database';
-    
-    $db = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
-?>
--- community/post_page/index.php
+++ community/post_page/index.php
@@ -1,3 +1,13 @@
+<?php
+  //connect to database
+  $conn = mysqli_connect('localhost', 'hexin', '123', 'csce_310_database');
+  
+  //check connection
+  if(!$conn){
+    echo 'Connection error: ' . mysqli_connect_error();
+  } 
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -7,13 +17,13 @@
   </head>
   <body>
     <h1>Community Page</h1>
-
+    <?php echo "Howdy!"?>
     <div class="create-post">
         <input id="input" type="text" placeholder="Create Post"  id="my-input" onclick="goToHomepage()">
     </div>
 
     <!-- TODO: Waiting for the database -->
-
+    
 
     <script src="index.js"></script>
   </body>
