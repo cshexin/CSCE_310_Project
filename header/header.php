e0882aeef56b2f7a702a2637f9ea125a3fba30e8
hexin223 <99561895+cshexin@users.noreply.github.com>
2023-04-19 16:25:02 -0500

    Create header.php

---
+++ header/header.php
@@ -0,0 +1,20 @@
+    <link rel="stylesheet" type="text/css" href="../../header/header.css">
+</head>
+
+<body>
+    <nav>
+        <div class="navbar-items">
+            <a class="nav-item" href="#">Profile</a>
+            <a class="nav-item" href="#">Community</a>
+        </div>
+
+        <a class="web-title" href="#">Howdy Healthy</a>
+        
+        <div class="navbar-items">
+            <a class="nav-item" href="#">Appointment</a>
+
+            <a class="nav-item" href="#">Log out</a>
+        </div>
+
+
+    </nav>
