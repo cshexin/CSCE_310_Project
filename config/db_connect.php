730af431c6872aea86a5af4976affec35b9a6d90
TianchengWu4810-tamu <tianchengwu4810@tamu.edu>
2023-04-20 08:45:54 -0500

    stop tracking db_connect.php

--- config/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-  //connect to database
-  $conn = mysqli_connect(/*fill user information from phpmyadmin */); 
-  
-  //check connection
-  if(!$conn){
-    echo 'Connection error: ' . mysqli_connect_error();
-  } 
-?>
