5d5e112c7483e7a8d13b28904f8c311bef6b79b1
TianchengWu4810-tamu <TianchengWu4810-tamu@github.com>
2023-04-19 19:17:14 -0500

    comment - ver0.3

--- community/comment_page/index.php
+++ community/comment_page/index.php
@@ -2,8 +2,22 @@
       // connect database
       include('../../config/db_connect.php');
 
+      // Check if the URL contains the "postid" parameter
+      if (!isset($_GET['postid'])) {
+        // Set the default "postid" value
+        $defaultPostId = 1; 
+
+        // Build a new URL, including the "postid" parameter
+        $newUrl = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . "?postid=" . $defaultPostId;
+
+        // Redirect to the new URL
+        header("Location: " . $newUrl);
+        exit();
+      }
+      
+      $post_id = $_GET['postid'];
+
       // Write query for all posts
-      $post_id = 1;
       $sql = "SELECT * FROM comment WHERE post_id = $post_id";
       $currentPostsql = "SELECT * FROM post WHERE post_id = $post_id";
 
