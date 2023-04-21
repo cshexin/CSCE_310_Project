f6486bd86e7e9da1ddc4ad1f0a9fa0ff767f5dff
Valerie <vvillafana09@gmail.com>
2023-04-21 16:46:44 -0500

    added php js and css files

---
+++ appointment/index.php
@@ -0,0 +1,23 @@
+<?php
+
+    
+
+?> 
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Appointment</title>
+    </head>
+    <body>
+        <h1>Appointment A</h1>
+        <label for="dname"> Doctor Name:</label>
+        <input type="text" id="dname" name="dname"> <br> <br>
+        <label for="pname"> Patient Name:</label>
+        <input type="text" id="pname" name="pname"> <br> <br>
+        <label for="meeting-time">Date and Time</label>
+        <input type="datetime-local" id="meeting-time" name="meeting-time"> <br> <br>
+        <button type="button">Edit</button>
+        <button type="button">Cancel</button>
+    </body>
+</html>
