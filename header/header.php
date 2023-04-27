85b8e3e91fc1a0eecaef3c2c352049d30890fce9
Valerie <vvillafana09@gmail.com>
2023-04-27 14:43:02 -0500

    updated header to direct to appointment page

--- header/header.php
+++ header/header.php
@@ -1,20 +1,16 @@
-    <link rel="stylesheet" type="text/css" href="../../header/header.css">
+<head>
+    <link rel="stylesheet" type="text/css" href="../header/header.css">
 </head>
-
 <body>
     <nav>
         <div class="navbar-items">
             <a class="nav-item" href="#">Profile</a>
-            <a class="nav-item" href="#">Community</a>
+            <a class="nav-item" href="/csce_310_project/community/post_page">Community</a>
         </div>
-
         <a class="web-title" href="#">Howdy Healthy</a>
         
         <div class="navbar-items">
-            <a class="nav-item" href="#">Appointment</a>
-
-            <a class="nav-item" href="#">Log out</a>
+            <a class="nav-item" href="/csce_310_project/appointment">Appointment</a>
+            <a class="nav-item" href="#">Logout</a>
         </div>
-
-
     </nav>
