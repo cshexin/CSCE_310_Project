2ddda4d617ca3528e96f98e394d58d9b21bfe213
Valerie <vvillafana09@gmail.com>
2023-04-28 18:11:10 -0500

    cleaned up code

--- appointments/dashboard/index.php
+++ appointments/dashboard/index.php
@@ -2,6 +2,7 @@
 
     // connect to database
     include("../../config/db_connect.php");
+    // replace when integrating
     $patientid = 1;
 
     // wrtite query for patients
@@ -15,11 +16,12 @@
     $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
     $datapatient = mysqli_fetch_all($resultPatient, MYSQLI_ASSOC);
 
-     // free result from memory
+    // free result from memory
     mysqli_free_result($result);
     mysqli_free_result($resultPatient);
 
     mysqli_close($conn);
+    // get the first and last name of patient
     $patientName = $datapatient[0]['first_name'] . ''. $datapatient[0]['last_name'];
     $cssFile = 'index.css';
     $cssContent = file_get_contents($cssFile);
@@ -28,7 +30,7 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang=eng>
     <head>
         <title>Dashboard</title>
         <link rel="stylesheet" type="text/css" href="index.css?version=<?php echo $hash; ?>">
@@ -37,16 +39,20 @@
         <?php include('../../header/header.php'); ?>
         <h1>Dashboard</h1>
         <div id="dashboard-title">
-            <h2> Hello, <?php echo $patientName;?></h2>
-            <form action="../appointment/create.php">
-                <button type="submit" id="add_app" name="add_app">Schedule an Appointment</button>
+            <h3> Hello, <?php echo $patientName;?></h3>
+            <form action="../appointment/create.php" method="post">
+                <button type="input" id="add_app" name="add_app" value='<?php echo $patientid; ?>'>
+                    <p>Schedule an Appointment<p>
+                </button>
             </form>
         </div>
         <div id="patient-appointments">
             <?php foreach($data as $d): ?>
             <div>
                 <form action="../appointment/index.php" method="post">
-                    <button type="submit" id="app_id" name="app_id_button" value="<?php echo $d['app_id']; ?>">Appointment</button>
+                    <button type="input" id="app_id_button" name="app_id_button" value="<?php echo $d['app_id']; ?>">
+                        <h2>Appointment</h2>
+                    </button>
                 </form>
             </div>
             <?php endforeach; ?>
