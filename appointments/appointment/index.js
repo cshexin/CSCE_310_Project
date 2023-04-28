5f40c5a520a17f56febe232f9c9b865cf4f0e0e0
Valerie <vvillafana09@gmail.com>
2023-04-28 08:30:47 -0500

    moved js file to a different folder

---
+++ appointments/appointment/index.js
@@ -0,0 +1,12 @@
+// When cancel button is pressed 
+// delete whole appointment
+function cancelAppointment(){
+
+
+}
+
+// when edit button is pressed
+// only able to change Date and Time
+function editAppointment(){
+
+}
