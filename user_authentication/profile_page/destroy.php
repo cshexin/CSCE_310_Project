c7cb7a815d12e38f1a4801f335240de0d4b30fae
Andrew Mao <andrewmao@Andrews-MacBook-Pro-8.local>
2023-05-01 16:26:06 -0500

    fixed delete user to work with doctors

--- user_authentication/profile_page/destroy.php
+++ user_authentication/profile_page/destroy.php
@@ -5,12 +5,16 @@ include('../../config/db_connect.php');
 if(!isset($_SESSION)) { session_start(); }
 
 $id = 0;
-$dob = $fname = $lname = $email = $password = "";
+$sql = "";
 
 if (!isset($_SESSION["id"])) {
     header("location: ../signin_page");
 } else {
-    $sql = "DELETE FROM patient WHERE p_id = {$_SESSION['id']}";
+    if($_SESSION['isPatient']){
+        $sql = "DELETE FROM patient WHERE p_id = {$_SESSION['id']}";
+    } else {
+        $sql = "DELETE FROM doctor WHERE d_id = {$_SESSION['id']}";
+    }
     $stmt = mysqli_prepare($conn, $sql);
     if (mysqli_stmt_execute($stmt)) {
         if(mysqli_stmt_affected_rows($stmt) != 1){
