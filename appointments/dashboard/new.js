aaaf49f386a04d0ae4a40be026c2f31d131329ac
Valerie <vvillafana09@gmail.com>
2023-04-30 19:50:59 -0500

    remove console.log output

--- appointments/dashboard/new.js
+++ appointments/dashboard/new.js
@@ -1,5 +1,4 @@
 document.getElementById('editButton').onclick = function() {
-    console.log("edit pressed");
     document.getElementById('dname').hidden = true;
     document.getElementById('showdoctorlist').hidden = false;
     document.getElementById('meeting-time').removeAttribute('readonly');
